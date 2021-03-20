f7f4927a8a7e14d4807d27047ede158470949bb5
Cybercrafter345 <49823186+Cybercrafter345@users.noreply.github.com>
2021-03-20 15:39:12 -0400

    Created Tiny search box

--- client/src/components/Charities.js
+++ client/src/components/Charities.js
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from'react';
-import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useParams, Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import Entry from './Entry';
-import { useParams } from 'react-router-dom'
+import axios from 'axios';
 
 function Posts() {
+  const [query, setQuery] = useState('');
+  const linkTo = `${query}`;
+
+  function handleChange(event) {
+    setQuery(event.target.value);
+  }
   var { q } = useParams();
   const [posts, setPosts] = useState([]);
   useEffect(() => {
@@ -14,13 +22,19 @@ function Posts() {
       .catch(err => {
         console.log(err)
       })
-  }, []);
+  }, [posts]);
   return (
-    <div className="Posts">
+    <div>
+      <form className="smallSearch">
+        <input type="text" placeholder="Search Waffles :D" onChange={handleChange} value={query}/>
+        <Link className="searchGlass" to={linkTo}><FontAwesomeIcon icon={faSearch}/></Link>
+      </form>
+      <div className="Posts">
       {posts.map(post => {
         console.log(post)
         return <Entry post={post}/>
       })}
+      </div>
     </div>
   );
 }
